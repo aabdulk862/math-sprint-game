d81525c44514c64296c9d31b7dd1cdb640cb719c
aabdulk862 <78934570+aabdulk862@users.noreply.github.com>
2022-07-25 16:37:58 -0400

    score page

--- script.js
+++ script.js
@@ -37,6 +37,37 @@ let finalTime = 0;
 let finalTimeDisplay = "0.0s";
 // Scroll
 let valueY = 0;
+// Reset Game
+function playAgain() {
+  gamePage.addEventListener("click", startTimer);
+  scorePage.hidden = true;
+  splashPage.hidden = false;
+  equationsArray = [];
+  playerGuessArray = [];
+  valueY = 0;
+  playAgainBtn.hidden = true;
+}
+// Show Score Page
+function showScorePage() {
+  // Show Play Again button after 1 second
+  setTimeout(() => {
+    playAgainBtn.hidden = false;
+  }, 1000);
+  gamePage.hidden = true;
+  scorePage.hidden = false;
+}
+// Format & Display Time in DOM
+function scoresToDOM() {
+  finalTimeDisplay = finalTime.toFixed(1);
+  baseTime = timePlayed.toFixed(1);
+  penaltyTime = penaltyTime.toFixed(1);
+  baseTimeEl.textContent = `Base Time: ${baseTime}`;
+  penaltyTimeEl.textContent = `Penalty: +${penaltyTime}`;
+  finalTimeEl.textContent = `${finalTimeDisplay}s`;
+  // Scroll to Top, go to Score page
+  itemContainer.scrollTo({ top: 0, behavior: "instant" });
+  showScorePage();
+}
 // Stop Timer, Process Results, go to score page
 function checkTime() {
   console.log(timePlayed);
@@ -61,6 +92,7 @@ function checkTime() {
       "final:",
       finalTime
     );
+    scoresToDOM();
   }
 }
 // Add a tenth of a second to timePlayed
